8dc402d66a154eaff7f4f7c566c1b3098a978ad1
Nguyên <nguyendangdcn@gmail.com>
2020-04-08 22:31:39 +0700

    Configure dependency injection using autofact.

---
+++ WebService_ASPNET_MVC_API/INT1448.WebApi/App_Start/IocConfigtion.cs
@@ -0,0 +1,28 @@
+﻿using Autofac;
+using Autofac.Integration.WebApi;
+using INT1448.EntityFramework.EntityFramework.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+using System.Web.Http;
+
+namespace INT1448.WebApi.App_Start
+{
+    public class IocConfigtion
+    {
+        public static void Configure()
+        {
+            var builder = new ContainerBuilder();
+
+            builder.RegisterApiControllers(Assembly.GetExecutingAssembly());
+
+            //builder.RegisterType<IRepository>().AsSelf().InstancePerDependency();
+
+            var container = builder.Build();
+            var resolver = new AutofacWebApiDependencyResolver(container);
+            GlobalConfiguration.Configuration.DependencyResolver = resolver;
+        }
+    }
+}
