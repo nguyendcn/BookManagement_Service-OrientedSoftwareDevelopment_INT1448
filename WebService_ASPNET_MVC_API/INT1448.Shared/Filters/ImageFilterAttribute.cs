5d015e1900750ef38c4f3cfa6d1b033c31015ad9
Nguyên <nguyendangdcn@gmail.com>
2020-04-14 22:40:31 +0700

    Add service to upload file default is upload image.

---
+++ WebService_ASPNET_MVC_API/INT1448.Shared/Filters/ImageFilterAttribute.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web.Http.Controllers;
+using System.Web.Http.Filters;
+
+namespace INT1448.Shared.Filters
+{
+    public class ImageFilterAttribute : ActionFilterAttribute
+    {
+        public override Task OnActionExecutingAsync(HttpActionContext actionContext, CancellationToken cancellationToken)
+        {
+            return Task.Factory.StartNew(async () =>
+            {
+                Dictionary<string, object> dict = new Dictionary<string, object>();
+                if (actionContext.Request.Content.IsMimeMultipartContent())
+                {
+                    var provider = actionContext.Request.Content.ReadAsMultipartAsync(cancellationToken).Result;
+                    foreach (var content in provider.Contents)
+                    {
+                        //Here logic to check extension, magic number and length.
+                        //If any error occurred then throw exception with HttpStatusCode
+                        var fileName = content.Headers.ContentDisposition == null ? string.Empty : content.Headers.ContentDisposition.FileName;
+                        var fileInBytes = content.ReadAsByteArrayAsync().Result;
+
+                        string ext = String.Empty;
+                        if (fileName.Contains("\""))
+                        {
+                            ext = fileName.Replace("\"", "");
+                        }
+
+                        ext = ext.Substring(fileName.LastIndexOf('.'));
+                        string extension = ext.ToLower();
+                        var validExtensions = new List<string>() { "jpg", "gif", "png" };
+                        if (!validExtensions.Contains(extension, StringComparer.OrdinalIgnoreCase))
+                        {
+                            var message = string.Format("Please Upload image of type .jpg,.gif,.png.");
+
+                            dict.Add("error", message);
+                            actionContext.Response = actionContext.Request.CreateResponse(HttpStatusCode.BadRequest, dict);
+                        }
+
+                        if (fileInBytes != null && fileInBytes.Any() && fileInBytes.Length >= 3000000)
+                        {
+                            var message = string.Format("Please Upload a file upto 3 mb.");
+
+                            dict.Add("error", message);
+                            actionContext.Response = actionContext.Request.CreateResponse(HttpStatusCode.BadRequest, dict);
+                        }
+                    }
+                }
+            }, cancellationToken);
+        }
+    }
+}
---
+++ WebService_ASPNET_MVC_API/INT1448.WebApi/Controllers/ImageManagerController.cs
@@ -0,0 +1,78 @@
+﻿using INT1448.Application.Storage;
+using INT1448.Shared.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+
+namespace INT1448.WebApi.Controllers
+{
+    [RoutePrefix("api/imagemanagers")]
+    public class ImageManagerController : ApiController
+    {
+        private IStorageService _storageService;
+
+        public ImageManagerController(IStorageService storageService)
+        {
+            _storageService = storageService;
+        }
+
+        [HttpPost]
+        [ImageFilterAttribute]
+        public HttpResponseMessage PostImage()
+        {
+            List<string> fileAdded = new List<string>();
+            Dictionary<string, object> dict = new Dictionary<string, object>();
+            try
+            {
+                var httpRequest = HttpContext.Current.Request;
+
+                if(httpRequest.Files.Count == 0)
+                {
+                    var res = string.Format("Please Upload a image.");
+                    dict.Add("error", res);
+                    return Request.CreateResponse(HttpStatusCode.NotFound, dict);
+                }
+
+                foreach (string file in httpRequest.Files)
+                {
+                    HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.Created);
+
+                    var postedFile = httpRequest.Files[file];
+                    if (postedFile != null && postedFile.ContentLength > 0)
+                    {
+                        var ext = postedFile.FileName.Substring(postedFile.FileName.LastIndexOf('.'));
+                        var extension = ext.ToLower();
+
+                        string myImageFileName = Guid.NewGuid() + extension;
+                        //  where you want to attach your imageurl
+
+                        //if needed write the code to update the table
+
+                        _storageService.SaveFileAsync(postedFile.InputStream, myImageFileName);
+                        fileAdded.Add(myImageFileName);
+                    }
+                }
+                var message1 = string.Format("Image Updated Successfully.");
+                return Request.CreateErrorResponse(HttpStatusCode.Created, message1);
+            }
+            catch (Exception ex)
+            {
+                if(fileAdded.Count != 0)
+                {
+                    foreach(string filename in fileAdded)
+                    {
+                        _storageService.DeleteFileAsync(filename);
+                    }
+                }
+
+                dict.Add("error", ex.Message);
+                return Request.CreateResponse(HttpStatusCode.NotFound, dict);
+            }
+        }
+    }
+}
